ba50a65c1cb9706333e421f513b75b5a7cf90f95
kirbyj3 <kirby18@up.edu>
2016-04-18 11:43:16 -0700

    jacob commit - timer working

--- app/src/main/java/edu/up/cs301/boggle/BoggleState.java
+++ app/src/main/java/edu/up/cs301/boggle/BoggleState.java
@@ -31,11 +31,16 @@ public class BoggleState extends GameState {
     private int playerTurn; //tells which players turn it is
     private int player1Score; //tracks the score of player1
     private int player2Score; //tracks the score of player2
+    //NETWORK
     private String[] currentWord = new String[2]; //the current word the player is making
     private boolean timer; //true if the timer is running, false if timer has stopped
-    private ArrayList<String> wordBank1; //the current words in the word bank for human
-    private ArrayList<String> wordBank2; //the current words in the word bank for opponent
-    private String[][] gameBoard = new String[4][4]; //array of all the letters on the board
+
+    private ArrayList<String> wordBank1; //the current words in the word bank
+    private ArrayList<String> wordBank2; //the current words in the word bank
+
+    private String[][] gameBoard1 = new String[4][4]; //array of all the letters on the board
+    private String[][] gameBoard2 = new String[4][4]; //array of all the letters on the board
+
     private boolean[][] visited = new boolean[4][4];//tells weather a tile has been searched already
     private int[][] selectedLetters = new int[20][2]; //list of selected letters positions
     private ArrayList<String> compUsedWords = new ArrayList<String>(); //list of all words computer uses in game
@@ -44,6 +49,7 @@ public class BoggleState extends GameState {
     private int curLetterRow; //current row of letter selected
     private int curLetterCol;
     private int secondsLeft;  //tells how much time is left on the timer
+    //private String compPrevWord = ""; //computer's previously played word
     private int gameOver; //determines if game is over
     public int arrayIndex;//int for the index, used in the Local Game
 
@@ -69,7 +75,7 @@ public class BoggleState extends GameState {
         curLetter = "a";
         curLetterRow = 4; //4 means null
         curLetterCol = 4; // 4 means null
-        secondsLeft = 180; // 3 minutes of play
+        secondsLeft = 30; // 3 minutes of play
 
         //Assigns random letters to the tiles
         Random r1 = new Random();
@@ -105,28 +111,28 @@ public class BoggleState extends GameState {
         Random r16 = new Random();
         char c16 = (char)(r16.nextInt(26) + 'A');
 
-        gameBoard[0][0] = ("" + c1);
-        gameBoard[1][0] = ("" + c2);
-        gameBoard[2][0] = ("" + c3);
-        gameBoard[3][0] = ("" + c4);
-        gameBoard[0][1] = ("" + c5);
-        gameBoard[1][1] = ("" + c6);
-        gameBoard[2][1] = ("" + c7);
-        gameBoard[3][1] = ("" + c8);
-        gameBoard[0][2] = ("" + c9);
-        gameBoard[1][2] = ("" + c10);
-        gameBoard[2][2] = ("" + c11);
-        gameBoard[3][2] = ("" + c12);
-        gameBoard[0][3] = ("" + c13);
-        gameBoard[1][3] = ("" + c14);
-        gameBoard[2][3] = ("" + c15);
-        gameBoard[3][3] = ("" + c16);
+        gameBoard1[0][0] = ("" + c1);
+        gameBoard1[1][0] = ("" + c2);
+        gameBoard1[2][0] = ("" + c3);
+        gameBoard1[3][0] = ("" + c4);
+        gameBoard1[0][1] = ("" + c5);
+        gameBoard1[1][1] = ("" + c6);
+        gameBoard1[2][1] = ("" + c7);
+        gameBoard1[3][1] = ("" + c8);
+        gameBoard1[0][2] = ("" + c9);
+        gameBoard1[1][2] = ("" + c10);
+        gameBoard1[2][2] = ("" + c11);
+        gameBoard1[3][2] = ("" + c12);
+        gameBoard1[0][3] = ("" + c13);
+        gameBoard1[1][3] = ("" + c14);
+        gameBoard1[2][3] = ("" + c15);
+        gameBoard1[3][3] = ("" + c16);
 
         //makes sure all Q tiles turn into QU tiles
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                if (gameBoard[i][j].equals("Q")) {
-                    gameBoard[i][j] = ("Qu");
+                if (gameBoard1[i][j].equals("Q")) {
+                    gameBoard1[i][j] = ("Qu");
                 }
             }
         }
@@ -155,14 +161,14 @@ public class BoggleState extends GameState {
         int vowelCount = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                if (gameBoard[i][j].equals("A") || gameBoard[i][j].equals("E") ||
-                        gameBoard[i][j].equals("I") || gameBoard[i][j].equals("O") || gameBoard[i][j].equals("U")) {
+                if (gameBoard1[i][j].equals("A") || gameBoard1[i][j].equals("E") ||
+                        gameBoard1[i][j].equals("I") || gameBoard1[i][j].equals("O") || gameBoard1[i][j].equals("U")) {
                     vowelCount++;
                 }
             }
         }
         if (vowelCount == 0) {
-            gameBoard[randomRow][randomCol] = randomVowel;
+            gameBoard1[randomRow][randomCol] = randomVowel;
         }
         //sets all the selected letters to null at start of game
        for (int k = 0; k < 20; k++) {
@@ -170,8 +176,12 @@ public class BoggleState extends GameState {
           selectedLetters[k][1] = 4;
         }
         wordBank1 = new ArrayList<String>();  // human used words
-        wordBank2 = new ArrayList<String>();  //opponants used words
-        //compUsedWords = new ArrayList<String>(); //computer used words
+        wordBank2 = new ArrayList<String>();
+
+        gameBoard2 = Arrays.copyOf(gameBoard1, gameBoard1.length);
+
+
+        compUsedWords = new ArrayList<String>(); //computer used words
     }
 
     /**
@@ -193,15 +203,26 @@ public class BoggleState extends GameState {
         wordBank2 = new ArrayList<String>();
 
         for (int i = 0; i < state.getWordBank(0).size(); i++) {
+
             wordBank1.add(state.wordBank1.get(i));
         }
+
+
         for (int i = 0; i < state.getWordBank(1).size(); i++) {
+
             wordBank2.add(state.wordBank2.get(i));
         }
+
+
+
+        compUsedWords = state.compUsedWords;
         timer = state.timer;
-        gameBoard = Arrays.copyOf(state.gameBoard, state.gameBoard.length);
-        visited = Arrays.copyOf(state.visited,state.gameBoard.length);
+        gameBoard1 = Arrays.copyOf(state.gameBoard1, state.gameBoard1.length);
+        gameBoard2 = Arrays.copyOf(state.gameBoard2, state.gameBoard2.length);
+        visited = Arrays.copyOf(state.visited,state.gameBoard1.length);
         selectedLetters = Arrays.copyOf(state.selectedLetters, state.selectedLetters.length);
+        //compPrevWord = state.compPrevWord;
+        //NETWORK
         this.currentWord = new String[2];
         for(int i = 0; i < 2;i ++){
             this.currentWord[i]= state.currentWord[i];
@@ -215,10 +236,25 @@ public class BoggleState extends GameState {
     public ArrayList<String> getCompUsedWords() {return compUsedWords;}
     public void setCompUsedWords(String word){compUsedWords.add(word);}
     public void setTimer(boolean timer) {this.timer = timer;}
-    public String[][] getGameBoard() {return Arrays.copyOf(gameBoard, gameBoard.length);}
-    public void setGameBoard(String[][] gameBoard){
-        this.gameBoard = Arrays.copyOf(gameBoard, gameBoard.length);
+    public String[][] getGameBoard(int playerNum) {
+
+        if (playerNum == 0) {
+            return Arrays.copyOf(gameBoard1, gameBoard1.length);
+        }
+        else {
+            return Arrays.copyOf(gameBoard2, gameBoard2.length);
+        }
     }
+//    public void setGameBoard(String[][] gameBoard, int playerNum){
+//        if (playerNum == 0) {
+//            this.gameBoard1 = Arrays.copyOf(gameBoard1, gameBoard.length);
+//        }
+//        else {
+//            return Arrays.copyOf(gameBoard2, gameBoard2.length);
+//        }
+//
+//
+//    }
     public int getWinner() {
         if (getPlayer1Score() > getPlayer2Score()) {return 1;}
         else if (getPlayer2Score()>getPlayer1Score()) {return 2;}
@@ -228,13 +264,20 @@ public class BoggleState extends GameState {
     public void setPlayer1Score(int player1Score) {this.player1Score = player1Score;}
     public int getPlayer2Score() {return player2Score;}
     public void setPlayer2Score(int player2Score) {this.player2Score = player2Score;}
+    //NETWORK
     public String getCurrentWord(int playerNum) {return currentWord[playerNum];}
     public void setCurrentWord(String currentWord, int playerNum) {this.currentWord[playerNum] = currentWord;}
     public ArrayList<String> getWordBank(int playerNum) {
-        if (playerNum == 0) {return wordBank1;
-        }else{return wordBank2;
+
+        if (playerNum == 0) {
+            return wordBank1;
+        }
+        else {
+            return wordBank2;
         }
     }
+//    public void setWordBank(ArrayList<String> wordBank) {
+//        this.wordBank = wordBank;}
     public HashSet<String> getDictionary(){return dictionary;}
     public int getGameOver(){return this.gameOver;}
     public void setGameOver(int gameOver) {this.gameOver = gameOver;}
@@ -509,7 +552,7 @@ public class BoggleState extends GameState {
      *
      * @param board two dimensional array representing the board
      */
-    public void rotateBoard(String[][] board) {
+    public void rotateBoard(String[][] board, int playerNum) {
         String[][] tmp = new String[4][4];
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 4; ++j) {
@@ -527,7 +570,12 @@ public class BoggleState extends GameState {
          */
 
         //Copies the rotated board to the existing board
-        gameBoard = tmp;
+        if (playerNum == 0) {
+            gameBoard1 = tmp;
+        }
+        else {
+            gameBoard2 = tmp;
+        }
     }
 
     /**
